5da5710edb3dfadc30808832c05dd613e22769dd
Ashley Matthews <ashleymatthews91@gmail.com>
2017-02-22 21:16:48 +0000

    Player builder now picks color randomly and removes it from color options

--- src/js/playerBuilder.js
+++ src/js/playerBuilder.js
@@ -2,21 +2,27 @@ var Player = require('./player');
 
 var PlayerBuilder = (function () {
 
-    this.colors;
+    this.availableColors;
 
-    this.colorIndex;
+    this.colorsInUse;
 
     this.game;
 
     function PlayerBuilder(game) {
         this.game = game;
         this.colorIndex = 0;
-        this.colors = ['0xFF9966', '0xDD3AAD', '0xDDF2EC', '0x528881'];
+        this.availableColors = ['0xFF9966', '0xDD3AAD', '0xDDF2EC', '0x528881'];
+        this.colorsInUse = [];
     }
 
     PlayerBuilder.prototype.generateNewPlayer = function(x, y) {
-        var player = new Player(this.game, x, y, 'Player', this.colors[this.colorIndex]);
-        this.colorIndex++;
+        var colorIndex = Math.floor(Math.random() * (this.availableColors.length));
+
+        var player = new Player(this.game, x, y, 'Player', this.availableColors[colorIndex]);
+
+        this.colorsInUse.push(this.availableColors[colorIndex]);
+        this.availableColors.splice(colorIndex, 1);
+
         return player;
     };
 
